bf4310adaf86ecc68d1124b549c1e9df337f5f47
pedro <elprogramadorpedro@gmail.com>
2025-07-18 15:56:47 -0300

    feat(location): add utilities for tracking and clearing user location updates
    Implemented , , and  to handle real-time location retrieval and cleanup. Supports high-accuracy geolocation and provides safe abstraction for location-based features.

--- src/actions/location/location.ts
+++ src/actions/location/location.ts
@@ -1,30 +1,44 @@
 import Geolocation from '@react-native-community/geolocation';
-import { Location } from '../../infrastructure/interfaces/locations';
+import {Location} from '../../infrastructure/interfaces/locations';
 
-export const getCurrentLocation=async():Promise<Location> => {
-
-
-return new Promise ((resolve, reject)=>{
-     
-   Geolocation.getCurrentPosition((info)=>{
-
+export const getCurrentLocation = async (): Promise<Location> => {
+  return new Promise((resolve, reject) => {
+    Geolocation.getCurrentPosition(
+      info => {
         resolve({
-            latitude: info.coords.latitude,
-            longitude: info.coords.longitude,
-        })
-
-     }, (error) => {
+          latitude: info.coords.latitude,
+          longitude: info.coords.longitude,
+        });
+      },
+      error => {
         console.log('Error getting location:', error);
         reject(error);
-     },
-     {
-       enableHighAccuracy: true, 
-     }
-    
-    
-    ) 
+      },
+      {
+        enableHighAccuracy: true,
+      },
+    );
+  });
+};
 
-})
+export const watchCurrentLocation = (
+  locationCallback: (location: Location) => void,
+): number => {
+  return Geolocation.watchPosition(
+    info =>
+      locationCallback({
+        latitude: info.coords.latitude,
+        longitude: info.coords.longitude,
+      }),
+    error => {
+      throw new Error(`Can't get watchPosition `);
+    },
+    {
+      enableHighAccuracy: true,
+    },
+  );
+};
 
-
-}
+export const clearWachLocation = (watchId: number) => {
+  Geolocation.clearWatch(watchId);
+};
