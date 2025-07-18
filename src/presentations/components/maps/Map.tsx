7a5c4e17f8447f155b1336e845b2b18f850b9377
pedro <elprogramadorpedro@gmail.com>
2025-07-18 15:33:46 -0300

    feat(map): add camera movement to user's current location on FAB press
    Implemented logic to animate the map camera to the user's current or last known location using  from the location store. Linked the behavior to a floating action button for intuitive user interaction.

--- src/presentations/components/maps/Map.tsx
+++ src/presentations/components/maps/Map.tsx
@@ -1,67 +1,77 @@
-import { Platform } from 'react-native'
-import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'
-import { StyleSheet } from 'react-native'
-import { Location } from '../../../infrastructure/interfaces/locations';
-import { FAB } from '../ui/FAB';
+import {Platform} from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import {StyleSheet} from 'react-native';
+import {Location} from '../../../infrastructure/interfaces/locations';
+import {FAB} from '../ui/FAB';
+import {useRef} from 'react';
+import {useLocationStore} from '../../store/location/useLocationStore';
 
-
-interface Props{
-    showsUserLocation: boolean;
-    initialLocation:Location;
+interface Props {
+  showsUserLocation: boolean;
+  initialLocation: Location;
 }
 
-export const Map = ({showsUserLocation=true, initialLocation}:Props) => {
+export const Map = ({showsUserLocation = true, initialLocation}: Props) => {
+  const mapRef = useRef<MapView>(null);
+  const cameraLocation = useRef<Location>(initialLocation);
+
+  const {getLocation, lastKnownLocation} = useLocationStore();
+
+  const moveCameraToLocation = (location: Location) => {
+    if (!mapRef.current) return;
+
+    mapRef.current.animateCamera({center: location});
+  };
+
+  const moveToCurrentLocation = async () => {
+    if(!lastKnownLocation){
+      moveCameraToLocation(initialLocation);
+    }
+
+    const location = await getLocation();
+    if (!location) return;
+    moveCameraToLocation(location);
+  };
+
   return (
     <>
-       <MapView
-           provider={ Platform.OS === 'ios' ? undefined: PROVIDER_GOOGLE} // remove if not using Google Maps
-           style={styles.map}
-           showsUserLocation={showsUserLocation}
-           region={{
-             latitude: initialLocation.latitude,
-             longitude: initialLocation.longitude,
-             latitudeDelta: 0.015,
-             longitudeDelta: 0.0121,
-           }}
-         >
-            
-    <Marker
-   coordinate={{
-             latitude: 10.48801,
-             longitude: -66.87919
+      <MapView
+        ref={map => {
+          mapRef.current = map!;
+        }}
+        showsUserLocation={showsUserLocation}
+        provider={Platform.OS === 'ios' ? undefined : PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: cameraLocation.current.latitude,
+          longitude: cameraLocation.current.longitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}>
+        <Marker
+          coordinate={{
+            latitude: 10.48801,
+            longitude: -66.87919,
+          }}
+          title="Marker Title"
+          description="Marker Description"
+        />
+      </MapView>
 
-            
-           }}
-
-              title="Marker Title"
-              description='Marker Description' 
-    />
-
-
-         </MapView>
-
-
-<FAB
-
-iconName='compass-outline'
-onPress={() => console.log('FAB Pressed')}
-style={{
-  bottom: 20,
-  right: 20,  
-}}
-
-/>
-  
-
-
-
-
+      <FAB
+        iconName="compass-outline"
+        onPress={moveToCurrentLocation}
+        style={{
+          bottom: 20,
+          right: 20,
+        }}
+      />
     </>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   map: {
     flex: 1,
   },
-})
+});
